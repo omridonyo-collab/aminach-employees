d24d84e657e6a731c1d83bb4e44b8b740c847bbc
omridonyo-collab <omridonyo@gmail.com>
2026-03-27 19:54:27 +0300

    Update emailService.ts

--- src/lib/emailService.ts
+++ src/lib/emailService.ts
@@ -2,7 +2,6 @@ import emailjs from '@emailjs/browser';
 import { FormSubmission, ApprovalStep } from '@/types';
 import { encodeFormToUrl } from './formUrlEncoder';
 
-// פרטי ההתחברות המעודכנים שלך
 const SERVICE_ID = 'service_pjriey4'; 
 const TEMPLATE_ID = 'template_9o9u06p'; 
 const PUBLIC_KEY = '5L86K9G1Oq7_XmNlI';
@@ -10,7 +9,6 @@ const HR_EMAIL = 'romi@aminach.co.il';
 
 export const sendApprovalRequestEmail = async (form: FormSubmission, nextStep: ApprovalStep) => {
   const formLink = encodeFormToUrl(form);
-  
   const templateParams = {
     to_email: nextStep.managerEmail,
     to_name: nextStep.managerName || nextStep.title,
@@ -19,13 +17,11 @@ export const sendApprovalRequestEmail = async (form: FormSubmission, nextStep: A
     form_link: formLink,
     reply_to: HR_EMAIL,
   };
-
   return emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams, PUBLIC_KEY);
 };
 
 export const sendHrFinalEmail = async (form: FormSubmission) => {
   const formLink = encodeFormToUrl(form);
-  
   const templateParams = {
     to_email: HR_EMAIL,
     to_name: "רומי",
@@ -34,6 +30,5 @@ export const sendHrFinalEmail = async (form: FormSubmission) => {
     form_link: formLink,
     reply_to: HR_EMAIL,
   };
-
   return emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams, PUBLIC_KEY);
 };
