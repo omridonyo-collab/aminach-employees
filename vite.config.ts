c912b34ecaa91aaf3a504762aabdec4408eb463c
omridonyo-collab <omridonyo@gmail.com>
2026-03-28 20:22:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/aminach-employees/',
   plugins: [react()],
+  // השורה הזו קריטית - היא אומרת ל-Vite שהאתר רץ תחת תיקיית aminach-employees
+  base: '/aminach-employees/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
